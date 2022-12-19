883f5837c3fa1238ec6c032baad445e9ab84ee90
kreytos23 <kreytos.pavi10@gmail.com>
2022-12-18 21:53:49 -0600

    Matriz 3x6 con interrupciones

--- MxCx3_6.ino
+++ MxCx3_6.ino
@@ -1,15 +1,15 @@
 //Pines
 int contadorBoton = 10;
 int configBoton = A0;
-int dip1 = 8;
-int dip2 = 9;
+int dip1 = 10;
+int dip2 = 11;
 
-int salida1 = 7;
-int salida2 = 6;
-int salida3 = 5;
-int salida4 = 4;
-int salida5 = 3;
-int salida6 = 2;
+int salida1 = 9;
+int salida2 = 8;
+int salida3 = 7;
+int salida4 = 6;
+int salida5 = 5;
+int salida6 = 4;
 
 int entrada1 = A1;
 int entrada2 = A2;
@@ -18,8 +18,8 @@ int entrada3 = A3;
 
 //Variables
 
-int contador = 0;
-int estadoConfig = 0;
+volatile int contador = 0;
+volatile int estadoConfig = 0;
 
 int bandera1Control1 = -1;
 int bandera2Control1 = -1;
@@ -49,45 +49,77 @@ void setup() {
   pinMode(salida5, OUTPUT);
   pinMode(salida6, OUTPUT);
   
-  pinMode(contadorBoton, INPUT);
-  pinMode(configBoton, INPUT);
+  attachInterrupt(digitalPinToInterrupt(2), cambiarEstadoConfig, RISING);
+  attachInterrupt(digitalPinToInterrupt(3), contadorIncremento, RISING);
+
   pinMode(entrada1, INPUT);
   pinMode(entrada2, INPUT);
   pinMode(entrada3, INPUT);
   pinMode(dip1, INPUT);
   pinMode(dip2, INPUT);
-  
 }
 
 void loop() {
-
-  if(digitalRead(contadorBoton) == HIGH){
-    contador++;
-    delay(300);
-  }
-  if(contador >= 7){
-    contador = 0;
-    delay(300);
-  }
-
+  Serial.print("Contador: ");
   Serial.println(contador);
+  Serial.print("Estado: " );
+  Serial.println(estadoConfig);
 
-  if(digitalRead(configBoton) == HIGH){
-     delay(300);
-    if(estadoConfig == 0){
-      estadoConfig = 1;
-    }else{
-      estadoConfig = 0;
-    }
+  habilitarSalidaConEntrada(bandera1Control1, bandera2Control1, salida1);
+  habilitarSalidaConEntrada(bandera1Control2, bandera2Control2, salida2);
+  habilitarSalidaConEntrada(bandera1Control3, bandera2Control3, salida3);
+  habilitarSalidaConEntrada(bandera1Control4, bandera2Control4, salida4);
+  habilitarSalidaConEntrada(bandera1Control5, bandera2Control5, salida5);
+  habilitarSalidaConEntrada(bandera1Control6, bandera2Control6, salida6);
+}
+
+int configurarSalida(int dip){
+  if(dip == 1){
+    return digitalRead(dip1);
+  }else{
+    return digitalRead(dip2);
   }
+}
 
+void contadorIncremento(){
+  delayMicroseconds(400);
+  contador++;
+  if(contador >= 7){
+    contador = 1;
+  }
+}
+
+void habilitarSalidaConEntrada(int bandera1, int bandera2, int salida){
+  if(bandera1 == 0 && bandera2 == 0){
+    for(int i = 0; i <= 255; i++){
+      digitalWrite(salida, digitalRead(entrada1));
+    }
+  }else if(bandera1 == 0 && bandera2 == 1){
+    for(int i = 0; i <= 255; i++){
+      digitalWrite(salida, digitalRead(entrada2));
+    }
+  }else if(bandera1 == 1 && bandera2 == 0){
+    for(int i = 0; i <= 255; i++){
+      digitalWrite(salida, digitalRead(entrada3));
+    } 
+  }else{
+    digitalWrite(salida, LOW);
+  }
+}
+
+void cambiarEstadoConfig(){
+  if(estadoConfig == 0){
+    estadoConfig = 1;
+  }else{
+    estadoConfig = 0;
+  }
+  
   if(estadoConfig == 1){
     switch(contador){
       case 1:
         Serial.println("Configurando 1");
         bandera1Control1 = configurarSalida(1);
         bandera2Control1 = configurarSalida(2);
-        
       break;
       case 2:
         Serial.println("Configurando 2");
@@ -116,41 +148,4 @@ void loop() {
       break;
     }
   }
-
-  if(contador == 0){
-    habilitarSalidaConEntrada(bandera1Control1, bandera2Control1, salida1);
-    habilitarSalidaConEntrada(bandera1Control2, bandera2Control2, salida2);
-    habilitarSalidaConEntrada(bandera1Control3, bandera2Control3, salida3);
-    habilitarSalidaConEntrada(bandera1Control4, bandera2Control4, salida4);
-    habilitarSalidaConEntrada(bandera1Control5, bandera2Control5, salida5);
-    habilitarSalidaConEntrada(bandera1Control6, bandera2Control6, salida6);
-  }
 }
-
-int configurarSalida(int dip){
-    if(dip == 1){
-      return digitalRead(dip1);
-    }else{
-      return digitalRead(dip2);
-    }
-}
-
-void habilitarSalidaConEntrada(int bandera1, int bandera2, int salida){
-   if(bandera1 == 0 && bandera2 == 0){
-      for(int i = 0; i <= 255; i++){
-        digitalWrite(salida, digitalRead(entrada1));
-      }
-    }else if(bandera1 == 0 && bandera2 == 1){
-      for(int i = 0; i <= 255; i++){
-        digitalWrite(salida, digitalRead(entrada2));
-      }
-    }else if(bandera1 == 1 && bandera2 == 0){
-      for(int i = 0; i <= 255; i++){
-        digitalWrite(salida, digitalRead(entrada3));
-      } 
-    }else{
-      digitalWrite(salida, LOW);
-    }
-}
-
-
